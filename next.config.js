a20cc9c82749c317a510e8fed4017fcfc92f088b
Behram <77900065+ikhanbehram@users.noreply.github.com>
2022-07-17 19:25:28 +0500

    action

--- next.config.js
+++ next.config.js
@@ -3,5 +3,7 @@ module.exports = {
         domains: ["res.cloudinary.com", "avatars.githubusercontent.com", "imgur.com"],
         loader: "akamai",
         path: ""
-    }
+    },
+    basePath: "/nextjs-pages",
+    assetPrefix: "/nextjs-pages"
 };
