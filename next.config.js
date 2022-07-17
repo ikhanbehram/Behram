9513e4657042b0f4a4d03327ec5eece01e86feea
Behram <77900065+ikhanbehram@users.noreply.github.com>
2022-07-17 19:28:38 +0500

    Revert "action"
    This reverts commit a20cc9c82749c317a510e8fed4017fcfc92f088b.

--- next.config.js
+++ next.config.js
@@ -3,7 +3,5 @@ module.exports = {
         domains: ["res.cloudinary.com", "avatars.githubusercontent.com", "imgur.com"],
         loader: "akamai",
         path: ""
-    },
-    basePath: "/nextjs-pages",
-    assetPrefix: "/nextjs-pages"
+    }
 };
